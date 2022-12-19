93b3889b98db66baa7348df728c10d2f450ac266
Oleksandr_Podoliako <Oleksandr_Podoliako@epam.com>
2022-12-19 23:48:53 +0200

    Improve ResponseWrapper

--- src/main/java/io/github/oleksandrpodoliako/nearest/apiwrappers/ResponseWrapper.java
+++ src/main/java/io/github/oleksandrpodoliako/nearest/apiwrappers/ResponseWrapper.java
@@ -2,12 +2,34 @@ package io.github.oleksandrpodoliako.nearest.apiwrappers;
 
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResponseWrapper<T> {
+
+    private String statusLine;
+    private Map<String, String> headers;
     private T body;
     private Response responseRaw;
 
     public int getStatusCode() {
         return responseRaw.getStatusCode();
+    }
+
+    public String getStatusLine() {
+        return statusLine;
+    }
+
+    public void setStatusLine(String statusLine) {
+        this.statusLine = statusLine;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
     }
 
     public T getBody() {
@@ -24,5 +46,13 @@ public class ResponseWrapper<T> {
 
     public void setResponseRaw(Response responseRaw) {
         this.responseRaw = responseRaw;
+        initResponseFields(responseRaw);
+    }
+
+    private void initResponseFields(Response responseRaw) {
+        setStatusLine(getResponseRaw().getStatusLine());
+        Map<String, String> headers = new HashMap<>();
+        responseRaw.headers().forEach(header -> headers.put(header.getName(), header.getValue()));
+        setHeaders(headers);
     }
 }
